7872962d1779aa65cc4f994db1425f1ecabf4680
AtixAisaka <Atix982@outlook.com>
2020-10-18 01:52:35 +0200

    AppServiceProvider Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\Events;
+use Calendar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +28,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        View::composer('layout', function ($view) {
+            $events = Events::get();
+            $event_list = [];
+            foreach ($events as $key => $event){
+                $event_list[] = Calendar::event(
+                    $event->event_name,
+                    true,
+                    new \DateTime($event->start_date),
+                    new \DateTime($event->end_date)
+                );
+            }
+
+            $view->with('calendar_details', Calendar::addEvents($event_list));
+        });
     }
 }

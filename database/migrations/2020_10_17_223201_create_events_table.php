a4434b894b0815cadceec12bf8b0e0124d51428a
AtixAisaka <Atix982@outlook.com>
2020-10-18 01:00:34 +0200

    Migrations Fix 2

--- database/migrations/2020_10_17_223201_create_events_table.php
+++ database/migrations/2020_10_17_223201_create_events_table.php
@@ -15,10 +15,10 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->timestamps();
             $table->string('event_name');
             $table->date('start_date');
             $table->date('end_date');
-            $table->timestamps();
         });
     }
 

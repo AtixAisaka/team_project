1bc00a1389830f8439782d0fc64086b2691c1910
RaizeN1337 <rnra1zen@gmail.com>
2020-11-16 20:09:40 +0100

    Edit event info + edit event tag

--- resources/views/events/editevent.blade.php
+++ resources/views/events/editevent.blade.php
@@ -1,28 +1,64 @@
 @extends("layouts.app")
 @section('content')
-    <div class="container">
-        <div class="row justify-content-md-center">
-        <div class="breadcrumb">
+    <link href="{{ asset('css/sol.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventview.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventbutton.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/detail_button.css')}}">
+    <script type="text/javascript" src="{{ asset('js/sol.js') }}"></script>
 
-                <form method="post" action="{{action('EventsController@updateEventAction', ["id" => $event->id])}}">
-                    Nazov eventu:<br>
-                    <input type="text" name="event_name" value="{{ $event->event_name }}"><br>
-                    Od:<br>
-                    <input type="text" name="start_date" value="{{ $event->start_date }}"><br>
-                    Do:<br>
-                    <input type="text" name="end_date" value="{{ $event->end_date }}"><br>
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                    <input type="hidden" name="param" value="{{$param}}">
-                    <input type="hidden" name="userid" value="{{$userid}}">
-                    <input type="hidden" name="admin" value="{{$admin}}">
-                    <a href="{{action('EventsController@eventTagInfoView', ["id" => $event->id])}}" target="_blank">edit tagov</a>
-                    <input type="submit" name="submit" value="Uložiť"><br>
-                </form>
-        </div>
-    </div>
+
+
+
+    <div class="container border border-dark">
+        <form method="post" action="{{action('EventsController@updateEventAction', ["id" => $event->id])}}">
+        <h2>Edit eventu</h2><br>
+            <div class="row">
+                <div class="col-xs-3 col-sm-3 col-md-3">
+                    <div class="form-group">
+                        <div class="">
+                            <label for="name"><b>Názov eventu</b></label><br>
+                            <input style="margin: 5px 0 22px 0; width: 100%; " class="form-control" type="text" name="event_name" value="{{ $event->event_name }}">
+                        </div>
+                        <div class="">
+                            <label for="name"><b>Od dátomu</b></label><br>
+                            <input class="form-control" type="date" name="start_date" value="{{ $event->start_date }}"><br>
+                        </div>
+                        <div class="">
+                            <label for="name"><b>Do dátomu</b></label><br>
+                            <input class="form-control" type="date" name="end_date" value="{{ $event->end_date }}"><br>
+                        </div>
+                        <div class=""><br>
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            <input class="btn effect01" type="submit" name="submit" value="Uložiť"><br>
+                            <a href="{{action('EventsController@eventTagInfoView', ["id" => $event->id])}}" target="_blank">edit tagov</a>
+                        </div>
+                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        <input type="hidden" name="param" value="{{$param}}">
+                        <input type="hidden" name="userid" value="{{$userid}}">
+                        <input type="hidden" name="admin" value="{{$admin}}">
+                    </div>
+                </div>
+
+            </div>
+            </form>
+
+
+
     </div>
 
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+
+
+    <script type="text/javascript">
+        $(function() {
+            // initialize sol
+            $('#tags0').searchableOptionList();
+        });
+    </script>
+
+
+
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
 @endsection
--- resources/views/events/eventTagInfo.blade.php
+++ resources/views/events/eventTagInfo.blade.php
@@ -1,41 +1,52 @@
 @extends("layouts.app")
 @section('content')
     <link href="{{ asset('css/sol.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventview.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventbutton.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/detail_button.css')}}">
     <script type="text/javascript" src="{{ asset('js/sol.js') }}"></script>
     <form method="post" action="{{action('EventsController@eventaddTagInfo')}}">
 
+        <div class="container border border-dark">
 
-            tagy
+            <h2>Zoznam tagov udalosti: {{$event->event_name}}</h2>
+            @foreach($tags as $row)
+            <li class="table-row">
+                <div class="col col-1">{{$row->name}}</div>
+                <div class="col col-3">  <a class="btn-1" href="{{action('EventsController@deletetagInfo',['id' => $row->id, 'idevent' => $event->id])}}" role="button" target="_blank">Vymazať</a></div>
+            </li>
+            @endforeach
+
+
+
+            <h3>Pridať tagy</h3>
+            <br>
+            <div class="col-xs-3 col-sm-3 col-md-3">
             <select id="tags0"  name="tag[]" multiple="multiple">
 
-                    @foreach($alltags as $row)
-                        <option value="{{$row->id}}">{{$row->name}}</option>
-                    @endforeach
+                @foreach($alltags as $row)
+                    <option value="{{$row->id}}">{{$row->name}}</option>
+                @endforeach
+
             </select>
+            </div>
+
+
+            <div class="col-xs-3 col-sm-3 col-md-3">
+            <input type="hidden" name="idevent" value="{{$event->id}}">
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <input class="btn effect01" type="submit" name="submit" value="Pridať tagy"><br>
+            </div>
+
+
+        </div>
 
 
 
-    &nbsp;<br/>
-        <input type="hidden" name="idevent" value="{{$event->id}}">
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
-        <input type="submit" name="submit" value="Pridať tagy"><br>
-    <table class="table">
-        <h3>Zoznam tagov udalosti: {{$event->event_name}}</h3>
-        <thead>
-        <tr>
-            <th scope="col">meno</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($tags as $row)
-            <tr>
-                <td>{{$row->name}}</td>
-                <td><a href="{{action('EventsController@deletetagInfo',['id' => $row->id, 'idevent' => $event->id])}}">Vymazať</a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+
+
+
+
         <script type="text/javascript">
             $(function() {
                 // initialize sol

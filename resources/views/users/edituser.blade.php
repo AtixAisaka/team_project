76cde9da222b6bad34dbe0bc19304536a6183f9d
mensot <davidzsembera@gmail.com>
2020-11-17 17:38:04 +0100

    user list view for admin, edit user view as admin

--- resources/views/users/edituser.blade.php
+++ resources/views/users/edituser.blade.php
@@ -1,32 +1,44 @@
 @extends("layouts.app")
 @section('content')
-    <div class="container">
-        <div class="row justify-content-md-center">
-        <div class="breadcrumb">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventbutton.css')}}">
 
-                <form method="post" action="{{action('UsersController@updateUserAction')}}">
-                    Meno:<br>
-                    <input type="text" name="name" value="{{ $user->name }}"><br>
-                    Email:<br>
-                    <input type="text" name="email" value="{{ $user->email }}"><br>
-                    Role:<br>
-                    <select id="role" name="role" class="form-control">
-                        <option value="0">Užívateľ</option>
-                        <option value="1">Poverený pracovník pracoviska</option>
-                        @if(Auth::User()->role >= 3)
-                        <option value="2">Referent fakulty</option>
-                        @endif
-                        @if(Auth::User()->role >= 4)
-                        <option value="3">Referent univerzity</option>
-                        <option value="4">Administrátor</option>
-                        @endif
-                    </select> <br>
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                    <input type="hidden" name="id" value="{{$user->id}}">
-                    <input type="submit" name="submit" value="Uložiť"><br>
-                </form>
-        </div>
-    </div>
+    <div class="container">
+        <form method="post" action="{{action('UsersController@updateUserAction')}}">
+            <h2>Edit user</h2><br>
+            <div class="row">
+                <div class="col-xs-3 col-sm-3 col-md-3">
+                    <div class="form-group">
+                        <div class="">
+                            <label for="name"><b>Name</b></label><br>
+                            <input style="margin: 5px 0 22px 0; width: 100%; " class="form-control" type="text" name="name" value="{{ $user->name }}">
+                        </div>
+                        <div class="">
+                            <label for="email"><b>Email</b></label><br>
+                            <input class="form-control" type="text" name="email" value="{{ $user->email }}"><br>
+                        </div>
+                        <div class="">
+                            <label for="role"><b>Role</b></label><br>
+                            <select id="role" name="role" class="form-control">
+                                <option value="0">Užívateľ</option>
+                                <option value="1">Poverený pracovník pracoviska</option>
+                                @if(Auth::User()->role >= 3)
+                                    <option value="2">Referent fakulty</option>
+                                @endif
+                                @if(Auth::User()->role >= 4)
+                                    <option value="3">Referent univerzity</option>
+                                    <option value="4">Administrátor</option>
+                                @endif
+                            </select>
+                        </div>
+                        <div class=""><br>
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            <input type="hidden" name="id" value="{{$user->id}}">
+                            <input class="btn effect01" type="submit" name="submit" value="Uložiť"><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
--- resources/views/users/userlist.blade.php
+++ resources/views/users/userlist.blade.php
@@ -1,53 +1,55 @@
 @extends("layouts.app")
 @section('content')
+    <link href="{{ asset('css/sol.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/previous_ongoing_next_eventbutton.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/user_list.css')}}">
+    <script type="text/javascript" src="{{ asset('js/sol.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
     <div class="container">
-        <div class="jumbotron">
-            <table>
-                @foreach($users as $row)
-                    <tr>
-                        <td> ID: {{$row-> id}}, &nbsp; Meno: {{$row-> name}}, &nbsp; </td>
-                        <td> Email: {{$row-> email}}, &nbsp;
-                            Rola:
-                                @if($row->role == 0) Používateľ
-                                @elseif($row->role == 1) Pracovník pracoviska
-                                @elseif($row->role == 2) Referent Fakulty
-                                @elseif($row->role == 3) Referent Univerzity
-                                @else Administrátor
-                                @endif
-                                 ({{$row-> role}}).</td>
-                        <td>
-                            <a class="btn btn-primary btn" href="{{ action("UsersController@showEditUser",
-                                    ["id" => $row -> id])  }}" role="button">Editovať údaje</a>
-                        </td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Pozrieť eventy
-                                    <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <li><a class="dropdown-item" href="{{ action("EventsController@showEventsHistory",
-                                ["value" => 0, "id" => $row->id, "admin" => 1])  }}">Minulé</a></li>
-                                    <li><a class="dropdown-item" href="{{ action("EventsController@showEventsHistory",
-                                ["value" => 1, "id" => $row->id, "admin" => 1])  }}">Súčasné</a></li>
-                                    <li><a class="dropdown-item" href="{{ action("EventsController@showEventsHistory",
-                                ["value" => 2, "id" => $row->id, "admin" => 1])  }}">Budúce</a></li>
-                                </ul>
+        <h2>Zoznam používateľov</h2>
+
+        <li class="table-header">
+            <div class="col col-1">ID</div>
+            <div class="col col-2">Name</div>
+            <div class="col col-3">Email</div>
+            <div class="col col-4">Role</div>
+            <div class="col col-5"></div>
+        </li>
+
+       {{-- style="color: rgb(76, 175, 80)--}}
+
+        @foreach($users as $row)
+
+                <li class="table-row">
+                    <div class="col col-1">{{$row-> id}}</div>
+                    <div class="col col-2">{{$row-> name}}</div>
+                    <div class="col col-3">{{$row-> email}}</div>
+                    <div class="col col-4">
+                        @if($row->role == 0) Používateľ
+                        @elseif($row->role == 1) Pracovník pracoviska
+                        @elseif($row->role == 2) Referent Fakulty
+                        @elseif($row->role == 3) Referent Univerzity
+                        @else Administrátor
+                        @endif
+                         ({{$row-> role}})
+                    </div>
+                    <div class="col col-5">
+                        <a class="btn-1" href="{{ action("UsersController@showEditUser",["id" => $row -> id])  }}" role="button" target="_blank">Edit</a>
+                        | <a class="btn-1" href="{{ action("UsersController@deleteUserAction",["id" => $row -> id])  }}" role="button" target="_blank">Delete</a>
+                        | <div class="dropdown">
+                            <button class="btn-1" style="background-color: white; border: white; color: rgb(30, 144, 255);size: auto">Events</button>
+                            <div class="dropdown-content">
+                                <a href="{{ action("EventsController@showEventsHistory",["value" => 0, "id" => $row->id, "admin" => 1])  }}">Minulé</a>
+                                <a href="{{ action("EventsController@showEventsHistory",["value" => 1, "id" => $row->id, "admin" => 1])  }}">Súčasné</a>
+                                <a href="{{ action("EventsController@showEventsHistory",["value" => 2, "id" => $row->id, "admin" => 1])  }}">Budúce</a>
                             </div>
-                        </td>
-                        <td>
-                            <a class="btn btn-primary btn" href="{{ action("UsersController@deleteUserAction",
-                                    ["id" => $row -> id])  }}" role="button">Mazať</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
+                        </div>
+                    </div>
+                </li>
+        @endforeach
+
     </div>
-
-
-
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
 @endsection
